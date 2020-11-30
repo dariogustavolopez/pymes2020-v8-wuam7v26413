b600ed2719372f77dbebdd84216a78e9431c5288
dariogustavolopez <dario.lopez@outlook.com.ar>
2020-11-30 20:40:12 -0300

    Initial commit

---
+++ src/app/models/persona.ts
@@ -0,0 +1,13 @@
+export class Persona
+{
+IdPersona: number;//(pk autoincremental)
+Nombre: string; //nchar(50); 
+FecNacimiento: Date;
+Limite: number; //int;
+};
+export const Personas:Persona[]=[
+
+
+
+];
+ 
